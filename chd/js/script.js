a9c378c9369ce1dd0e633cdf428cff1e389f3af0
Guilherme Zacarias de Andrade <guilhermeza360@gmail.com>
2026-06-23 23:34:02 -0300

    jornada do usuário

---
+++ chd/js/script.js
@@ -0,0 +1,14 @@
+
+var fundo = document.getElementById("main")
+var imagem = document.getElementById("imagem")
+
+function hos () {
+    fundo.style.backgroundImage = "url('imagens/fundovermelho.png')"
+    imagem.style.backgroundImage = "url('imagens/hospital-desenho.png')"
+    
+}
+
+function fun () {
+    fundo.style.backgroundImage = "url('imagens/fundoazul.png')"
+    imagem.style.backgroundImage = "url('imagens/funcionario.png')"
+}
